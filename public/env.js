2ea79d1e53a35c6a63dd02274a6f0a468279e1c4
HungTran170904 <tienhung17092004@gmail.com>
2025-12-07 19:45:32 +0700

    feat: update env.js

---
+++ public/env.js
@@ -0,0 +1,10 @@
+window.env = {
+      "USER_SERVICE_ENDPOINT": "http://192.168.153.129:30080/user-service",
+      "TEAM_SERVICE_ENDPOINT": "http://192.168.153.129:30080/team-service",
+      "CHAT_SERVICE_ENDPOINT": "http://192.168.153.129:30080/chat-service",
+      "MEETING_SERVICE_ENDPOINT": "http://192.168.153.129:30080/meeting-service",
+      "WEBSOCKET_SERVICE_ENDPOINT": "ws://192.168.153.129:30080/websocket-service",
+      "AWS_REGION": "us-east-1",
+      "COGNITO_USER_POOL_ID": "us-east-1_y30eiJyjf",
+      "COGNITO_CLIENT_ID": "4u22a3844h3m93q0q6b87m4idl"
+};
